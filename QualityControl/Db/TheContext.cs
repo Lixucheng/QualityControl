d524e0fded6477fbd550a06c532f96a2c584ba44
lext <937505373@qq.com>
2015-08-26 16:08:47 +0800

    开始code first

---
+++ QualityControl/Db/TheContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace QualityControl.Db
+{
+    public class TheContext : DbContext
+    {
+        public TheContext()
+            :base("name=TheContext"){}
+
+
+    }
+}
--- QualityControl/Models/IdentityModels.cs
+++ QualityControl/Models/IdentityModels.cs
@@ -1,8 +1,9 @@
-﻿using System.Data.Entity;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
 
 namespace QualityControl.Models
 {
@@ -30,4 +31,14 @@ namespace QualityControl.Models
             return new ApplicationDbContext();
         }
     }
+
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole() : base() { }
+        public ApplicationRole(string name) : base(name) { }
+        public string Description { get; set; }
+    }
+
+
+
 }
