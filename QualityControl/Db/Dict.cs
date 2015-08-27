6c51121c9eac8f18054e9c19ec20ee39bcbfdbee
lext <937505373@qq.com>
2015-08-27 21:10:55 +0800

    漏了两个文件

---
+++ QualityControl/Db/Dict.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QualityControl.Db
+{
+    public class Dict
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+
+        public int Type { get; set; }
+
+        public int Status { get; set; }
+    }
+
+    public enum EnumDictType
+    {
+
+    }
+}
---
+++ QualityControl/Db/RegisterApply.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QualityControl.Db
+{
+    public class RegisterApply
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Time { get; set; }
+
+    }
+}
