bcfdcf7e3bc127f2763f2b5cbb6527537ba2c42f
lxc <572362732@qq.com>
2015-09-09 23:25:14 +0800

    产品种类

---
+++ QualityControl/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QualityControl.Controllers
+{
+    public class ProductController : BaseController
+    {
+        // GET: Product
+        /// <summary>
+        /// 产品类别界面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult TypeIndex()
+        {
+            var list = Db.ProductTypes.ToList();
+            list.ForEach(e => {
+                if(e.Description.Length>40)
+                e.Description = e.Description.Substring(0, 40) + "...";
+            });
+            ViewBag.list = list;
+            ViewBag.count = list.Count;
+            return View();
+        }
+
+        public JsonResult GetDescription(long id)
+        {                
+            return Json(Db.ProductTypes.Find(id).Description,JsonRequestBehavior.AllowGet); 
+        }
+    }
+}
