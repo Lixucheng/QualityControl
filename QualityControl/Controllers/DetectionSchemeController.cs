b131ddbd49f0f21505a8c84d6958b24d69a2c28f
lxc <572362732@qq.com>
2015-09-14 23:33:50 +0800

    啦啦啦

--- QualityControl/Controllers/DetectionSchemeController.cs
+++ QualityControl/Controllers/DetectionSchemeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using QualityControl.Db;
 
 namespace QualityControl.Controllers
@@ -13,40 +14,47 @@ namespace QualityControl.Controllers
         // GET: DetectionScheme
         public ActionResult BuildDetectionScheme()
         {
-            //var x = new Db.DetectionScheme();
-            //x.Level = "特殊检测水平 S-1 A";
-            //x.MaxQuote = 5000;
-            //x.MinQuote = 1000;
-            //x.MaxTime = 20;
-            //x.MinTime = 2;
-            //x.ProductId = 1;
-            //x.Status = QualityControl.Db.EnumDetectionSchemeStatus.未发送;
-            //x.CheckNum = Guid.NewGuid().ToString();
-            //Db.DetectionSchemes.Add(x);
-            //Db.SaveChanges();
-            //var list = new List<ProductBatch>();
-            //for (int i = 1; i < 5; i++)
-            //{
-            //    list.Add(new ProductBatch
-            //    {
-            //        BatchName="2015-8-"+i.ToString(),
-            //        Count=i*1000,
-            //        ProductId=1,
-            //CheckNum=x.CheckNum
-            //    });
-            //}
-            //Db.ProductBatchs.AddRange(list);
-            //Db.SaveChanges();
+            var x = new Db.DetectionScheme();
+            x.Level = "特殊检测水平 S-1 A";
+            x.MaxQuote = 5000;
+            x.MinQuote = 1000;
+            x.MaxTime = 20;
+            x.MinTime = 2;
+            x.ProductId = 1;
+            x.Status = QualityControl.Db.EnumDetectionSchemeStatus.未发送;
+            x.CheckNum = Guid.NewGuid().ToString();
+            Db.DetectionSchemes.Add(x);
+            Db.SaveChanges();
+            var list = new List<ProductBatch>();
+            for (int i = 1; i < 5; i++)
+            {
+                list.Add(new ProductBatch
+                {
+                    BatchName = "2015-8-" + i.ToString(),
+                    Count = i * 1000,
+                    ProductId = 1,
+                    CheckNum = x.CheckNum
+                });
+            }
+            Db.ProductBatchs.AddRange(list);
+            Db.SaveChanges();
 
-            //ViewBag.productname = Db.Products.Find(x.ProductId).Name;
-            //ViewBag.company = "百事公司";
-            //ViewBag.model = x;
-            //ViewBag.list = list;
+//            var pro = Db.Products.Find(x.ProductId);
+//            var
+//            company = Db.Companies.FirstOrDefault(e => e.UserId == pro.UserId);
+            ViewBag.productname = "pro.Name";
+            ViewBag.company = "company.Name";
+
+         
+            ViewBag.model = x;
+            ViewBag.list = list;
             return View();
         }
 
         public ActionResult SignContract(string checknum)
         {
+            //todo: 判断用户
+
             var x=Db.DetectionSchemes.FirstOrDefault(e => e.CheckNum == checknum && e.Status == EnumDetectionSchemeStatus.已发送待确定);
             ViewBag.model = x;
             var pro = Db.Products.Find(x.ProductId);
@@ -56,6 +64,62 @@ namespace QualityControl.Controllers
             ViewBag.company = company.Name;
             var list = Db.ProductBatchs.Where(e => e.CheckNum == checknum).ToList();
             ViewBag.list = list;
+
+            var user = UserManager.FindById(User.Identity.GetUserId());
+
+            var c = new Contract
+            {
+                CheckNum = checknum,
+                Level = x.Level,
+                MaxTime = x.MaxTime,
+                MinTime = x.MinTime,
+                ProductId = x.ProductId,
+                Quote = x.UserQuote,
+                Status = EnumContractStatus.已签定,
+                UserId = "123123"
+            };
+            Db.Contracts.Add(c);
+            Db.SaveChanges();
+
+            return View();
+        }
+
+        /// <summary>
+        /// 将合同发送给两方
+        /// </summary>
+        /// <param name="checknum"></param>
+        /// <param name="quser"></param>
+        /// <param name="qother"></param>
+        /// <returns></returns>
+        public ActionResult SendDetectionScheme(string checknum,double quser,double qother)
+        {
+            var x = Db.DetectionSchemes.FirstOrDefault(e => e.CheckNum == checknum && e.Status == EnumDetectionSchemeStatus.未发送);
+            x.UserQuote = quser;
+            x.OrganQuote = qother;
+            x.Status = EnumDetectionSchemeStatus.已发送待确定;
+            Db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Sign(string checknum)
+        {
+            var x = Db.Contracts.FirstOrDefault(e => e.CheckNum == checknum && e.Status == EnumContractStatus.未签订);
+            x.Status = EnumContractStatus.已签定;
+            Db.SaveChanges();
+            var x1= Db.DetectionSchemes.FirstOrDefault(e => e.CheckNum == checknum && e.Status==EnumDetectionSchemeStatus.已发送待确定);
+            x1.Status = EnumDetectionSchemeStatus.已确定;
+            Db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Modify(string checknum, string modify)
+        {
+            var x = Db.Contracts.FirstOrDefault(e => e.CheckNum == checknum && e.Status == EnumContractStatus.未签订);
+            x.Status = EnumContractStatus.修改后未审核;
+            Db.SaveChanges();
+            var x1 = Db.DetectionSchemes.FirstOrDefault(e => e.CheckNum == checknum && e.Status == EnumDetectionSchemeStatus.已发送待确定);
+            x1.Status = EnumDetectionSchemeStatus.已确定;
+            Db.SaveChanges();
             return View();
         }
     }
--- QualityControl/Db/DetectionScheme.cs
+++ QualityControl/Db/DetectionScheme.cs
@@ -25,15 +25,15 @@ namespace QualityControl.Db
 
         public int MaxTime { get; set; }       //检测时间 几天
 
-        public int MaxQuote { get; set; }      //报价
+        public double MaxQuote { get; set; }      //报价
 
         public int MinTime { get; set; }       //检测时间 几天
 
-        public int MinQuote { get; set; }      //报价
+        public double MinQuote { get; set; }      //报价
 
-        public int UserQuote { get; set; }     //给用户的报价
+        public double UserQuote { get; set; }     //给用户的报价
 
-        public int OrganQuote { get; set; }    //给检测机构的报价
+        public double OrganQuote { get; set; }    //给检测机构的报价
 
         public EnumDetectionSchemeStatus Status { get; set; }
     }
@@ -47,19 +47,22 @@ namespace QualityControl.Db
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
 
-        public string Project { get; set; }    //检查项目
+        public string CheckNum { get; set; }  //检测号
+
+        public long ProductId { get; set; }  //检查产品
+
 
         public string Level { get; set; }      //级别
 
         public int MaxTime { get; set; }       //检测时间 几天
 
-        public int MaxQuote { get; set; }      //报价
-
+    
         public int MinTime { get; set; }       //检测时间 几天
 
-        public int MinQuote { get; set; }      //报价
+        public double Quote { get; set; }      //报价
 
         public string UserId { get; set; }     //用户
+
 
         public EnumContractStatus Status { get; set; } //状态
 
@@ -79,7 +82,8 @@ namespace QualityControl.Db
         未发送=0,
         已发送待确定=1,
         返回修改=2,
-        修改完成留档保存=3
+        修改完成留档保存=3,
+        已确定
     }
 
     /// <summary>
