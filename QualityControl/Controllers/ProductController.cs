fcc6bf5488226702feb4fac816a72beecf463ee4
lxc <572362732@qq.com>
2015-09-20 13:09:39 +0800

    table

--- QualityControl/Controllers/ProductController.cs
+++ QualityControl/Controllers/ProductController.cs
@@ -8,89 +8,11 @@ namespace QualityControl.Controllers
 {
     public class ProductController : BaseController
     {
+
+
         #region 产品种类
-        #region  第三级种类部分
-        // GET: Product
-        /// <summary>
-        /// 产品类别界面
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult TypeIndex()
-        {
-            var list = Db.ThirdProductTypes.ToList();
-            list.ForEach(e => {
-                if(e.Description.Length>40)
-                e.Description = e.Description.Substring(0, 40) + "...";
-            });
-            ViewBag.list = list;
-            ViewBag.count = list.Count;
-            return View();
-        }
-  
-        public ActionResult Edit(Db.ThirdProductType newone)
-        {
-            if (!CheckNewProduct(newone))
-            {
-               throw new Exception("存在重复或有字段为空，请检查后再输入");
-            }
-            var x = Db.ThirdProductTypes.Find(newone.Id);
-            if(x!=null)
-            {
-                x.Title = newone.Title;
-                x.Description = newone.Description;
-                Db.SaveChanges();
-            }
-            else { throw new Exception("不存在此产品");}
-            return Redirect("./TypeIndex");
-        }
+       
 
-        public ActionResult Add(Db.ThirdProductType newone)
-        {
-            if (!CheckNewProduct(newone))
-            {
-                throw new Exception("存在重复或有字段为空，请检查后再输入");
-            }
-            Db.ThirdProductTypes.Add(newone);
-            Db.SaveChanges();
-            return Redirect("./TypeIndex");
-        }
-
-        public ActionResult Del(long id)
-        {
-            var x = Db.ThirdProductTypes.Find(id);
-            if (x != null)
-            {
-                Db.ThirdProductTypes.Remove(x);
-                Db.SaveChanges();
-            }
-            return Redirect("../TypeIndex");
-        }
-
-        public ActionResult _Options()
-        {
-            var thislist=Db.ThirdProductTypes.ToList();
-            ViewBag.thislist = thislist;
-            ViewBag.thiscount = thislist.Count;
-            return View();
-        }
-
-
-        #region productType 逻辑函数
-
-        public bool CheckNewProduct(Db.ThirdProductType newone)
-        {
-            if (string.IsNullOrEmpty(newone.Title) || string.IsNullOrEmpty(newone.Description))
-            {
-                return false;
-            }
-            var x = Db.ThirdProductTypes.Count(e => e.Title == newone.Title&&e.Id!=newone.Id);
-            return x <= 0;
-        }
-
-
-
-        #endregion
-        #endregion
         #region 第一级
         public ActionResult FirstTypeIndex()
         {
@@ -104,11 +26,126 @@ namespace QualityControl.Controllers
             return View();
         }
 
+        public ActionResult FirstEdit(Db.FirstProductType newone)
+        {
+            if (!CheckNewProduct(newone))
+            {
+                throw new Exception("存在重复或有字段为空，请检查后再输入");
+            }
+            var x = Db.FirstProductTypes.Find(newone.Id);
+            if (x != null)
+            {
+                x.Title = newone.Title;
+                x.Description = newone.Description;
+                Db.SaveChanges();
+            }
+            else { throw new Exception("不存在此产品"); }
+            return Redirect("./FirstTypeIndex");
+        }
+
+        public ActionResult FirstAdd(Db.FirstProductType newone)
+        {
+            if (!CheckNewProduct(newone))
+            {
+                throw new Exception("存在重复或有字段为空，请检查后再输入");
+            }
+            Db.FirstProductTypes.Add(newone);
+            Db.SaveChanges();
+            return Redirect("./FirstTypeIndex");
+        }
 
 
         #endregion
 
         #region 第二级
+        public ActionResult SecondTypeIndex(long id)
+        {
+            var list = Db.FirstProductTypes.Find(id).SecondProductTypes;
+            list.ForEach(e => {
+                if (e.Description.Length > 40)
+                    e.Description = e.Description.Substring(0, 40) + "...";
+            });
+            ViewBag.list = list;
+            ViewBag.count = list.Count;
+            return View();
+        }
+
+        public ActionResult SecondEdit(Db.SecondProductType newone)
+        {
+            if (!CheckNewProduct(newone))
+            {
+                throw new Exception("存在重复或有字段为空，请检查后再输入");
+            }
+            var x = Db.SecondProductTypes.Find(newone.Id);
+            if (x != null)
+            {
+                x.Title = newone.Title;
+                x.Description = newone.Description;
+                Db.SaveChanges();
+            }
+            else { throw new Exception("不存在此产品"); }
+            return Redirect("./SecondTypeIndex");
+        }
+                                        
+        public ActionResult SecondAdd (Db.SecondProductType newone,long fid)
+        {
+            if (!CheckNewProduct(newone))
+            {
+                throw new Exception("存在重复或有字段为空，请检查后再输入");
+            }
+            Db.FirstProductTypes.Find(fid).SecondProductTypes.Add(newone);
+            Db.SaveChanges();
+            return Redirect("./SecondTypeIndex");
+        }
+        #endregion
+
+        #region  第三级
+        // GET: Product
+        /// <summary>
+        /// 产品类别界面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult TypeIndex(long id)
+        {
+            var list = Db.SecondProductTypes.Find(id).Productypes;
+            list.ForEach(e => {
+                if (e.Description.Length > 40)
+                    e.Description = e.Description.Substring(0, 40) + "...";
+            });
+            ViewBag.list = list;
+            ViewBag.count = list.Count;
+            return View();
+        }
+
+        public ActionResult Edit(Db.ThirdProductType newone)
+        {
+            if (!CheckNewProduct(newone))
+            {
+                throw new Exception("存在重复或有字段为空，请检查后再输入");
+            }
+            var x = Db.ThirdProductTypes.Find(newone.Id);
+            if (x != null)
+            {
+                x.Title = newone.Title;
+                x.Description = newone.Description;
+                Db.SaveChanges();
+            }
+            else { throw new Exception("不存在此产品"); }
+            return Redirect("./TypeIndex");
+        }
+
+        public ActionResult Add(Db.ThirdProductType newone,long fid)
+        {
+            if (!CheckNewProduct(newone))
+            {
+                throw new Exception("存在重复或有字段为空，请检查后再输入");
+            }
+            Db.SecondProductTypes.Find(fid).Productypes.Add(newone);
+            Db.SaveChanges();
+            return Redirect("./TypeIndex");
+        }
+
+
 
         #endregion
 
@@ -150,11 +187,116 @@ namespace QualityControl.Controllers
             }
           
         }
-        #endregion
+
+
+        public ActionResult Del(long id,int type)
+        {
+            switch (type)
+            {
+                case 1:
+                {
+                        var x = Db.FirstProductTypes.Find(id);
+                        if (x != null)
+                        {
+                            Db.FirstProductTypes.Remove(x);
+                            Db.SaveChanges();
+                        }
+                        return Redirect("./FirstTypeIndex");
+                    }
+                case 2:
+                {
+                        var x = Db.SecondProductTypes.Find(id);
+                        if (x != null)
+                        {
+                            Db.SecondProductTypes.Remove(x);
+                            Db.SaveChanges();
+                        }
+                        return Redirect("./SecondTypeIndex");
+                    }
+                case 3:
+                {
+                        var x = Db.ThirdProductTypes.Find(id);
+                        if (x != null)
+                        {
+                            Db.ThirdProductTypes.Remove(x);
+                            Db.SaveChanges();
+                        }
+                        return Redirect("./ThirdTypeIndex");
+                    }
+                default:
+                {
+                    throw new Exception("访问错误！");
+                }
+            }
+          
+        }
+
+
+        public ActionResult _Options(int type,long fatherid)
+        {
+
+            switch (type)
+            {
+                case 1:
+                {
+                    var thislist = Db.FirstProductTypes.ToList();
+                    ViewBag.thislist = thislist;
+                    ViewBag.thiscount = thislist.Count;
+                        break;
+                }
+                case 2:
+                {
+                    var thislist = Db.FirstProductTypes.Find(fatherid).SecondProductTypes;
+                    ViewBag.thislist = thislist;
+                    ViewBag.thiscount = thislist.Count;
+                        break;
+                    }
+                case 3:
+                    {
+                        var thislist = Db.SecondProductTypes.Find(fatherid).Productypes;
+                        ViewBag.thislist = thislist;
+                        ViewBag.thiscount = thislist.Count;
+                        break;
+                    }
+                default:
+                    throw new Exception("访问种类错误！");
+            }
+
+            
+            return View();
+        }
+
+        public bool CheckNewProduct(Db.ThirdProductType newone)
+        {
+            if (string.IsNullOrEmpty(newone.Title) || string.IsNullOrEmpty(newone.Description))
+            {
+                return false;
+            }
+            var x = Db.ThirdProductTypes.Count(e => e.Title == newone.Title && e.Id != newone.Id);
+            return x <= 0;
+        }
+        public bool CheckNewProduct(Db.FirstProductType newone)
+        {
+            if (string.IsNullOrEmpty(newone.Title) || string.IsNullOrEmpty(newone.Description))
+            {
+                return false;
+            }
+            var x = Db.ThirdProductTypes.Count(e => e.Title == newone.Title && e.Id != newone.Id);
+            return x <= 0;
+        }
+        public bool CheckNewProduct(Db.SecondProductType newone)
+        {
+            if (string.IsNullOrEmpty(newone.Title) || string.IsNullOrEmpty(newone.Description))
+            {
+                return false;
+            }
+            var x = Db.ThirdProductTypes.Count(e => e.Title == newone.Title && e.Id != newone.Id);
+            return x <= 0;
+        }
 
         #endregion
 
-
+        #endregion
 
 
         #region  生产商产品部分
