0707b42d0d8222407f0b5c6aed2a7ffb7a489760
波风水门 <572362732@qq.com>
2015-10-27 15:28:39 +0800

    aaaa

--- QualityControl/Controllers/ManufacturerController.cs
+++ QualityControl/Controllers/ManufacturerController.cs
@@ -4,6 +4,8 @@ using System;
 using Microsoft.AspNet.Identity;
 using QualityControl.Enum;
 using System.Data.Entity;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace QualityControl.Controllers
 {
@@ -33,12 +35,17 @@ namespace QualityControl.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Uploader(long id)
         {
             var trade = Db.Trades.Find(id);
             if(trade==null)
             {
                 throw new Exception("交易不存在！");
+            }
+            if(trade.Status!=(int)EnumTradeStatus.FinishMakeQrCode)
+            {
+                throw new Exception("访问错误！");
             }
             var uid=User.Identity.GetUserId();
             var user = UserManager.FindById(uid);
@@ -48,6 +55,22 @@ namespace QualityControl.Controllers
             }
 
             return View(trade);
+        }
+
+        [HttpPost]
+        public ActionResult Uploader(long id, List<string> list)
+        {
+            var trade = Db.Trades.Find(id);
+            if (trade == null)
+            {
+                throw new Exception("交易不存在！");
+            }
+
+            var files = JsonConvert.SerializeObject(list);
+            trade.Files = files;
+            trade.Status = (int)EnumTradeStatus.SampleStart;
+            Db.SaveChanges();
+            return Redirect("/Trade/TradeDetail/"+id);
         }
 
         public bool Finish(long id)
