ab8cde104114bd592faf690d5fa08d6ea3df575f
波风水门 <572362732@qq.com>
2015-10-30 10:25:01 +0800

    aaa

--- QualityControl/Migrations/Configuration.cs
+++ QualityControl/Migrations/Configuration.cs
@@ -1,17 +1,18 @@
-using System.Data.Entity.Migrations;
-using QualityControl.Models;
-
 namespace QualityControl.Migrations
 {
-    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<QualityControl.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
-            ContextKey = "QualityControl.Models.ApplicationDbContext";
         }
 
-        protected override void Seed(ApplicationDbContext context)
+        protected override void Seed(QualityControl.Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
